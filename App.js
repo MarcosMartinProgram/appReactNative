45a45b729c24c7d3e8d40f06b6d3db318eae3a56
marcos <martinmarcospablo@gmail.com>
2024-08-01 15:22:55 -0300

    Terminando de probar componentes

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView } from 'react-native';
+import icon from './assets/icon.png'
+import { useEffect, useState } from 'react';
+import { getLatestGames } from './lib/metacritic';
 
 export default function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <SafeAreaView style={{ margin: 20}}>
+
+        <ScrollView>
+          {games.map(game => (
+            <View key={game.slug} style={styles.card}>
+              <Image source={{ uri: game.image }} style={styles.image} />
+              <Text style={styles.title}>{game.title}</Text>
+              <Text style={styles.description}>{game.description}</Text>
+              <Text style={styles.score}>{game.score}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+      
     </View>
   );
 }
@@ -13,8 +37,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
   },
+  card: {
+    marginBottom: 42,
+    alignItems: 'center',
+  },
+  image: {
+    width: 107,
+    height: 147,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#fff",
+  },
+  description: {
+    fontSize: 16,
+    color: "#eee",
+    textAlign: "justify"
+
+  },
+  score: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "green",
+    marginTop: 10,
+  }
 });
