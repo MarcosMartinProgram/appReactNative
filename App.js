50af38b04a719bedb167768e76e2931a4a9c5790
marcos <martinmarcospablo@gmail.com>
2024-08-01 21:01:27 -0300

    Creación componentes

--- App.js
+++ App.js
@@ -1,72 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView } from 'react-native';
-import icon from './assets/icon.png'
-import { useEffect, useState } from 'react';
-import { getLatestGames } from './lib/metacritic';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Main from "./components/Main";
+
+import GameCard from "./components/GameCard";
 
 export default function App() {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    getLatestGames().then((games) => {
-      setGames(games);
-    });
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <SafeAreaView style={{ margin: 20}}>
-
-        <ScrollView>
-          {games.map(game => (
-            <View key={game.slug} style={styles.card}>
-              <Image source={{ uri: game.image }} style={styles.image} />
-              <Text style={styles.title}>{game.title}</Text>
-              <Text style={styles.description}>{game.description}</Text>
-              <Text style={styles.score}>{game.score}</Text>
-            </View>
-          ))}
-        </ScrollView>
-      </SafeAreaView>
-      
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  card: {
-    marginBottom: 42,
-    alignItems: 'center',
-  },
-  image: {
-    width: 107,
-    height: 147,
-    borderRadius: 10,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 10,
-    color: "#fff",
-  },
-  description: {
-    fontSize: 16,
-    color: "#eee",
-    textAlign: "justify"
-
-  },
-  score: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "green",
-    marginTop: 10,
-  }
 });
---
+++ components/Main.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { getLatestGames } from "../lib/metacritic";
+import { FlatList, ScrollView, View, ActivityIndicator } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context"; 
+import AnimatedGameCard from "./GameCard";
+import { Logo } from "./Logo";
+
+function Main() {
+  const [games, setGames] = useState([]);
+  const insets = useSafeAreaInsets();
+
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    });
+  }, []);
+  return (
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <Logo />
+      {games.length === 0 ? (
+        <ActivityIndicator color={"#fff"} size={"large"} />
+      ) : (
+        <FlatList
+          data={games}
+          keyExtractor={(game) => game.slug}
+          renderItem={({ item, index }) => (
+            <AnimatedGameCard game={item} index={index} />
+          )}
+        />
+      )}
+    </View>
+  );
+}
+
+export default Main;
